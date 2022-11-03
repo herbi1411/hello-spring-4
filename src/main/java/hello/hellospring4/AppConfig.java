b87bf021f6f05efbc9bba1ad5ad7eccdb9e9e81b
윤민수 <minsoo9615@naver.com>
2022-11-03 17:56:14 +0900

    jdbc memberRepository Error

--- src/main/java/hello/hellospring4/AppConfig.java
+++ src/main/java/hello/hellospring4/AppConfig.java
@@ -1,8 +1,24 @@
 package hello.hellospring4;
 
+import hello.hellospring4.repository.JdbcMemberRepository;
+import hello.hellospring4.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class AppConfig {
+    private final DataSource dataSource;
 
+    @Autowired
+    public AppConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new JdbcMemberRepository(dataSource);
+    }
 }
--- src/main/java/hello/hellospring4/service/MemberService.java
+++ src/main/java/hello/hellospring4/service/MemberService.java
@@ -4,6 +4,7 @@ import hello.hellospring4.domain.Member;
 import hello.hellospring4.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,7 +27,7 @@ public class MemberService {
     private void validDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
-                    throw new IllegalAccessException("이미 존재하는 회원입니다.")
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
     }
 
